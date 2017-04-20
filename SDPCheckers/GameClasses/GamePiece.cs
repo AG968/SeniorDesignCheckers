cceff613e52b5e95cec721a508dbac4c13b9a637
John Vasquez <jvasquez864@gmail.com>
2017-04-20 16:59:43 -0400

    Added KING checker piece movement and piece images

--- SDPCheckers/GameClasses/GamePiece.cs
+++ SDPCheckers/GameClasses/GamePiece.cs
@@ -8,17 +8,25 @@ namespace SDPCheckers.GameClasses
 {
     public class GamePiece
     {
+
         public enum Player
         {
             PLAYER1,
             PLAYER2
         }
+        public enum PieceType
+        {
+            CHECKER,
+            KING
+        }
 
         public Player player;
-   
+        public PieceType pieceType;
         public GamePiece(Player player)
         {
             this.player = player;
+            pieceType = PieceType.CHECKER;
         }
+
     }
 }
